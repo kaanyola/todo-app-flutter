6966df429712c14c681dc0be1c6f50ee63cfbf05
kaanyola <kaanyola@gmail.com>
2019-08-11 21:40:05 +0300

    form added

---
+++ lib/todo_form.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_flutter/todo.dart' as prefix0;
+
+class TodoForm extends StatefulWidget {
+
+  List<int> todoFormList;
+
+  @override
+  _TodoFormState createState() => _TodoFormState();
+}
+
+class _TodoFormState extends State<TodoForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
