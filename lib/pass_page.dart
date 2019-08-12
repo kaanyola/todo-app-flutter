5237aa1e334df02f1f6449e95b983080ebca107e
kaanyola <kaanyola@gmail.com>
2019-08-12 23:05:18 +0300

    pass page added

---
+++ lib/pass_page.dart
@@ -0,0 +1,13 @@
+class PassPage extends StatefulWidget {
+  @override
+  _PassPageState createState() => _PassPageState();
+}
+
+class _PassPageState extends State<PassPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
